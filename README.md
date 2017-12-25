7495aa31f9e49d2bac78dc1e5d8c06252318dc63
Sandro Wong <saloiofun@gmail.com>
2017-12-25 14:59:45 -0800

    Add Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Hangman-Game
-Hangman Game
+
+This Hangman Game is based on Street Fighter's characters
+Link: https://saloiofun.github.io/Hangman-Game/
+
+## Getting Started
+
+Use keyboard keys to guess letters
+
+## The following is displayed on the page:
+
+*  Wins: (# of times user guessed the word correctly).
+
+If the word is akuma, it is displayed like this when the game starts: _ _ _ _ _.
+
+As the user guesses the correct letters, reveal them: a k _ _ a.
+
+* Number of Guesses Remaining: (# of guesses remaining for the user).
+
+* Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
+
+After the user wins/loses the game should automatically choose another word and make the user play it.
+
+A song will be played when the user guesses their word correctly
+
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,230 +1,227 @@
 // Fighter Constructor
-function Fighter(name, nickname, fightStyle, sound, image) {
-	this.name = name;
-	this.nickname = nickname;
-	this.fightStyle = fightStyle;
-	this.sound = sound;
-	this.image = image;
+function Fighter (name, nickname, fightStyle, sound, image) {
+  this.name = name
+  this.nickname = nickname
+  this.fightStyle = fightStyle
+  this.sound = sound
+  this.image = image
 }
 
 // Street Fighter Characters
 var akuma = new Fighter(
-	"Akuma",
-	"Akuma",
-	"Satsui no Hado, Ansatsuken",
-	"https://www.youtube.com/embed/hTD3LhS7TXE?",
-	"akuma.png"
-	);
+  'Akuma',
+  'Akuma',
+  'Satsui no Hado, Ansatsuken',
+  'https://www.youtube.com/embed/hTD3LhS7TXE?',
+  'akuma.png'
+)
 
 var chunLi = new Fighter(
-	"Chun-Li",
-	"Chun-Li",
-	"Chinese martial arts",
-	"https://www.youtube.com/embed/EWk3Pm6M3-I?",
-	"chunLi.png"
-	);
+  'Chun-Li',
+  'Chun-Li',
+  'Chinese martial arts',
+  'https://www.youtube.com/embed/EWk3Pm6M3-I?',
+  'chunLi.png'
+)
 
 var dan = new Fighter(
-	"Dan Hibiki",
-	"Dan",
-	"Saikyo \"Ultimate\" Style",
-	"https://www.youtube.com/embed/lmSYC2N8sbs?",
-	"dan.png"
-	);
+  'Dan Hibiki',
+  'Dan',
+  'Saikyo "Ultimate" Style',
+  'https://www.youtube.com/embed/lmSYC2N8sbs?',
+  'dan.png'
+)
 
 var dhalsim = new Fighter(
-	"Dhalsim",
-	"Dhalsim",
-	"Esoteric Yoga",
-	"https://www.youtube.com/embed/P8zF56ol5Gs?",
-	"dhalsim.png"
-	);
+  'Dhalsim',
+  'Dhalsim',
+  'Esoteric Yoga',
+  'https://www.youtube.com/embed/P8zF56ol5Gs?',
+  'dhalsim.png'
+)
 
 var ken = new Fighter(
-	"Ken Masters",
-	"Ken",
-	"Ansatsuken",
-	"https://www.youtube.com/embed/Je7u4onLVC8?",
-	"ken.png"
-	);
+  'Ken Masters',
+  'Ken',
+  'Ansatsuken',
+  'https://www.youtube.com/embed/Je7u4onLVC8?',
+  'ken.png'
+)
 
 var ryu = new Fighter(
-	"Ryu",
-	"Ryu",
-	"Ansatsuken",
-	"https://www.youtube.com/embed/swPlzzavzWg?",
-	"ryu.png"
-	);
+  'Ryu',
+  'Ryu',
+  'Ansatsuken',
+  'https://www.youtube.com/embed/swPlzzavzWg?',
+  'ryu.png'
+)
 
 var sakura = new Fighter(
-	"Sakura Kasugano",
-	"Sakura",
-	["Imitation of Ansatsuken Karate"],
-	"https://www.youtube.com/embed/2gWGbmGD7r8?",
-	"sakura.png"
-	);
+  'Sakura Kasugano',
+  'Sakura',
+  'Imitation of Ansatsuken Karate',
+  'https://www.youtube.com/embed/2gWGbmGD7r8?',
+  'sakura.png'
+)
 
 var elFuerte = new Fighter(
-	"El Fuerte",
-	"El Fuerte",
-	"Lucha Libre",
-	"https://www.youtube.com/embed/FDrBjA9SCPI?",
-	"elFuerte.png"
-	);
+  'El Fuerte',
+  'El Fuerte',
+  'Lucha Libre',
+  'https://www.youtube.com/embed/FDrBjA9SCPI?',
+  'elFuerte.png'
+)
 
 var guile = new Fighter(
-	"Guile",
-	"Guile",
-	"Martial arts and professional wrestling",
-	"https://www.youtube.com/embed/Q6XM3fDUxLg?",
-	"guile.png"
-	);
+  'Guile',
+  'Guile',
+  'Martial arts and professional wrestling',
+  'https://www.youtube.com/embed/Q6XM3fDUxLg?',
+  'guile.png'
+)
 
 var feiLong = new Fighter(
-	"Fei Long",
-	"Fei Long",
-	"Kung Fu",
-	"https://www.youtube.com/embed/kTLwCx6spkU?",
-	"feiLong.png"
-	);
+  'Fei Long',
+  'Fei Long',
+  'Kung Fu',
+  'https://www.youtube.com/embed/kTLwCx6spkU?',
+  'feiLong.png'
+)
 
 // Initialize Variables
-var fighters = [akuma, chunLi, dan, dhalsim, ken, ryu, sakura, elFuerte, guile, feiLong];
-var validChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var fighters = [akuma, chunLi, dan, dhalsim, ken, ryu, sakura, elFuerte, guile, feiLong]
+var validChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-var fighter = fighters[Math.floor(Math.random() * fighters.length)];
-var wrongChars = [];
-var numberOfTries = fighter.nickname.length + 5;
-var fighterChar = fighter.nickname.toUpperCase().split(''); // Splits fighter's nickname into chars and returns an array
-var hiddenChar = hideChars(fighter, validChars, fighterChar);
-var wins = 0;
-
+var fighter = fighters[Math.floor(Math.random() * fighters.length)]
+var wrongChars = []
+var numberOfTries = fighter.nickname.length + 5
+var fighterChar = fighter.nickname.toUpperCase().split('') // Splits fighter's nickname into chars and returns an array
+var hiddenChar = hideChars(fighter, validChars, fighterChar)
+var wins = 0
 
 // Function to create guess lines for the game
-function hideChars(fighterObj, validCharsArray, fighterCharArray) {
-	var lines = [];
-	for (var i = 0; i < fighterObj.nickname.length; i++) {
-		if (validCharsArray.includes(fighterCharArray[i])) {
-			lines.push("_");
-		} else if (fighterCharArray[i] === " ") {
-			lines.push("&nbsp;");
-		} 
-		else {
-			lines.push(fighterCharArray[i]);
-		}
-	}
-	return lines;
+function hideChars (fighterObj, validCharsArray, fighterCharArray) {
+  var lines = []
+  for (var i = 0; i < fighterObj.nickname.length; i++) {
+    if (validCharsArray.includes(fighterCharArray[i])) {
+      lines.push('_')
+    } else if (fighterCharArray[i] === ' ') {
+      lines.push('&nbsp;')
+    } else {
+      lines.push(fighterCharArray[i])
+    }
+  }
+  return lines
 }
 
-function innerHtmlById(id, add){
-	document.getElementById(id).innerHTML = add;
+function innerHtmlById (id, add) {
+  document.getElementById(id).innerHTML = add
 }
 
-function reload() {
-	location.reload();
+function reload () {
+  location.reload()
 }
 
 // replaces a line by a char
-var replaceLine = function(array, index, char) {
-	array[index] = char;
-};
+var replaceLine = function (array, index, char) {
+  array[index] = char
+}
 
 // function to play fighter's theme
-var playTheme = function(fighterObj) {
-	var fighterThemeDiv = document.getElementById("fighterTheme");
-	fighterThemeDiv.className = "hideTheme";
-	fighterTheme.innerHTML = "";
+var playTheme = function (fighterObj) {
+  var fighterThemeDiv = document.getElementById('fighterTheme')
+  fighterThemeDiv.className = 'hideTheme'
+  fighterThemeDiv.innerHTML = ''
 
-	var iframeElement = document.createElement("iframe");
-	iframeElement.src = fighterObj.sound + "&autoplay=1";
+  var iframeElement = document.createElement('iframe')
+  iframeElement.src = fighterObj.sound + '&autoplay=1'
 
-	fighterThemeDiv.appendChild(iframeElement);
-} 
+  fighterThemeDiv.appendChild(iframeElement)
+}
 
 // function to display fighter's image
-var fighterImage = function(fighterObj) {
-	var fighterImageDiv = document.getElementById("fighterImage");
-	fighterImageDiv.innerHTML = "";
-	var imageElement = document.createElement("img");
-	imageElement.src = "assets/images/" + fighterObj.image;
-	imageElement.className = "img-responsive fighterPortrait"; // for Bootstrap
-	imageElement.alt = fighterObj.nickname;
+var fighterImage = function (fighterObj) {
+  var fighterImageDiv = document.getElementById('fighterImage')
+  fighterImageDiv.innerHTML = ''
+  var imageElement = document.createElement('img')
+  imageElement.src = 'assets/images/' + fighterObj.image
+  imageElement.className = 'img-responsive fighterPortrait' // for Bootstrap
+  imageElement.alt = fighterObj.nickname
 
-	var h1Element = document.createElement("h1");
-	h1Element.className = "fighterNickname";
-	h1Element.innerHTML = fighterObj.nickname;	
+  var h1Element = document.createElement('h1')
+  h1Element.className = 'fighterNickname'
+  h1Element.innerHTML = fighterObj.nickname
 
-	fighterImageDiv.appendChild(imageElement);
-	fighterImageDiv.appendChild(h1Element);
+  fighterImageDiv.appendChild(imageElement)
+  fighterImageDiv.appendChild(h1Element)
 }
 
-var resetGame = function(fightersObj) {
-	// Randomly chooses a choice from the fighters array.
-	fighter = fightersObj[Math.floor(Math.random() * fightersObj.length)];
-	fighterChar = fighter.nickname.toUpperCase().split('');
+var resetGame = function (fightersObj) {
+// Randomly chooses a choice from the fighters array.
+  fighter = fightersObj[Math.floor(Math.random() * fightersObj.length)]
+  fighterChar = fighter.nickname.toUpperCase().split('')
 
-	// Reset variables
-	wrongChars = [];
-	hiddenChar = hideChars(fighter, validChars, fighterChar);
-	numberOfTries = fighter.nickname.length + 5;
+// Reset variables
+  wrongChars = []
+  hiddenChar = hideChars(fighter, validChars, fighterChar)
+  numberOfTries = fighter.nickname.length + 5
 }
 
-innerHtmlById("guessesRemaining",numberOfTries);
-innerHtmlById("gameWins","Wins: " + wins);
-innerHtmlById("currentWord",hiddenChar.join(" "));
+innerHtmlById('guessesRemaining', numberOfTries)
+innerHtmlById('gameWins', 'Wins: ' + wins)
+innerHtmlById('currentWord', hiddenChar.join(' '))
 
 // receives user's input and checks if key exists in the fighterChar array
-document.onkeyup = function(event) {
-	var key = event.key.toUpperCase();
+document.onkeyup = function (event) {
+  var key = event.key.toUpperCase()
 
-	// if key exists in the figther array and number of tries is greater than 0, replaces the line by the key
-	// otherwise stores the wrong key and decrements number of tries
-	if (validChars.includes(key) && fighterChar.includes(key) && numberOfTries > 0) {
-		for(var i = 0; i < fighterChar.length; i++) {
-			if(fighterChar[i] === key) {
-				replaceLine(hiddenChar, i, key);
-			}
-		}
-	} else {
-		if (validChars.includes(key) && !wrongChars.includes(key) && numberOfTries > 0) {
-			wrongChars.push(key);
-			numberOfTries--;
-		}
-	}
+  // if key exists in the figther array and number of tries is greater than 0, replaces the line by the key
+  // otherwise stores the wrong key and decrements number of tries
+  if (validChars.includes(key) && fighterChar.includes(key) && numberOfTries > 0) {
+    for (var i = 0; i < fighterChar.length; i++) {
+      if (fighterChar[i] === key) {
+        replaceLine(hiddenChar, i, key)
+      }
+    }
+  } else {
+    if (validChars.includes(key) && !wrongChars.includes(key) && numberOfTries > 0) {
+      wrongChars.push(key)
+      numberOfTries--
+    }
+  }
 
-	// if number of tries reaches 0 
-	if (numberOfTries === 0) {
-		resetGame(fighters);
-	}
+  // if number of tries reaches 0
+  if (numberOfTries === 0) {
+    resetGame(fighters)
+  }
 
-	// if there are no more lines to guess
-	if (!hiddenChar.includes("_")) {
-		wins++;
+  // if there are no more lines to guess
+  if (!hiddenChar.includes('_')) {
+    wins++
 
-		playTheme(fighter);
-		fighterImage(fighter);
-		var index = fighters.indexOf(fighter);
-		fighters.splice(index, 1);
+    playTheme(fighter)
+    fighterImage(fighter)
+    var index = fighters.indexOf(fighter)
+    fighters.splice(index, 1)
 
-		if (fighters.length > 0) {
-			resetGame(fighters);
-		} else {
-			//win game and reset button
-			innerHtmlById("game", "");
-			innerHtmlById("game", "<h1>Thank you for Playing</h1><h1>You <span>W</span>in</h1>");
-			
-			var resetBtn = document.createElement("button");
-			resetBtn.className = "btn btn-primary";
-			resetBtn.setAttribute("type", "button");
-			resetBtn.onclick = reload;
-			resetBtn.innerHTML = "Reset Game";
-			document.getElementById("game").appendChild(resetBtn);
-		}		
+    if (fighters.length > 0) {
+      resetGame(fighters)
+    } else {
+      // win game and reset button
+      innerHtmlById('game', '')
+      innerHtmlById('game', '<h1>Thank you for Playing</h1><h1>You <span>W</span>in</h1>')
 
-	}
+      var resetBtn = document.createElement('button')
+      resetBtn.className = 'btn btn-primary'
+      resetBtn.setAttribute('type', 'button')
+      resetBtn.onclick = reload
+      resetBtn.innerHTML = 'Reset Game'
+      document.getElementById('game').appendChild(resetBtn)
+    }
+  }
 
-	innerHtmlById("guessesRemaining",numberOfTries);
-	innerHtmlById("gameWins","Wins: " + wins);
-	innerHtmlById("wrongGuesses",wrongChars.join(", "));
-	innerHtmlById("currentWord",hiddenChar.join(" "));
+  innerHtmlById('guessesRemaining', numberOfTries)
+  innerHtmlById('gameWins', 'Wins: ' + wins)
+  innerHtmlById('wrongGuesses', wrongChars.join(', '))
+  innerHtmlById('currentWord', hiddenChar.join(' '))
 }
